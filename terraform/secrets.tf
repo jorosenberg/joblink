5d0d278ef6bf0c38fc97e27e4b628a2eb4c2ff2c
Jonah Rosenberg <jrosenberg272@gmail.com>
2026-03-03 01:30:17 -0500

    update rds tf  name in secret

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -9,7 +9,7 @@ resource "aws_secretsmanager_secret_version" "db_credentials" {
   secret_string = jsonencode({
     username = var.db_username
     password = var.db_password
-    host     = aws_db_instance.postgres.address
+    host     = aws_db_instance.jobsdb.address
     port     = 5432
     dbname   = "jobscraper"
   })
